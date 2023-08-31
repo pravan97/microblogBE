c82f08763c005e15746e9caaf8b761bf3b0f9597
pravan97 <pravansuresh97@gmail.com>
2023-08-31 13:20:23 +0530

    microblogBE

--- microblog_backend/settings.py
+++ microblog_backend/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
 ]
 AUTH_USER_MODEL = 'api.CustomUser'
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
